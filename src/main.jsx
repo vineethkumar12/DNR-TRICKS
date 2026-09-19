ae8b075e13f7bc27b3884621d7bd790b10ccdbc6
vineethkumar12 <your-adepuvineethkumarvinni@gmail.com>
2026-09-19 15:03:46 +0530

    updated home page url

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './assets/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/DNR-TRICKS/">
       <AuthProvider>
         <App />
       </AuthProvider>
